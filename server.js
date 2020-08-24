4484832c23846bb63dc1c1bfb5f48bcd7eddc571
AhmadHirthani <hertani86@gmail.com>
2020-08-24 12:58:21 +0300

    fixInsertProblem

--- server.js
+++ server.js
@@ -74,7 +74,9 @@ function jokesFavPage(req, res) {
 function addJokeToList(req, res) {
     let { id, type, setup, punchline } = req.body;
     let VALUES = [id, type, setup, punchline];
-    let SQL = `INSERT INTO jokes (id, type, setup, punchline) VALUES(${id}, ${type}, ${setup}, ${punchline});`;
+    let SQL = `INSERT INTO jokes (id, type, setup, punchline)
+     VALUES( ${id} , ${type} , ${setup} , ${punchline} );`;
+    console.log('sql: ', SQL);
     // let SQL = `INSERT INTO jokes (id, type, setup, punchline) VALUES (${id}, ${type}, ${setup}, ${punchline});`;
     // console.log('id: ', id);
     client.query(SQL).then(data => {
